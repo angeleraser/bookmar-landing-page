2e3f808b3ed79f0c3e9d6a6115bcfcb37fc6c2d8
Angel Figuera <angel.figuera@osanasalud.com>
2021-02-08 15:55:02 -0400

    chore: add new plugin item in eslintrc config

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/images/png/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		Hola
 	</div>
 </template>
 
 <script>
 import '@/assets/styles/App.css';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
 	name: 'App',
-	components: {
-		HelloWorld,
-	},
 };
 </script>
 
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
+<style></style>
