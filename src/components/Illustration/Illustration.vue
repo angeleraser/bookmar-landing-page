1c03b4a0dbfeb28a32b8eb009d7c430e2e40dc17
Angel Figuera <angel.figuera@osanasalud.com>
2021-02-10 16:50:10 -0400

    fix: fix illustration image sizes in features components

--- src/components/Illustration/Illustration.vue
+++ src/components/Illustration/Illustration.vue
@@ -33,8 +33,13 @@ export default {
 }
 
 .illustration-wrapper .img {
+	display: inline-block;
 	z-index: 1;
 	height: 100%;
+}
+
+.illustration-wrapper img {
+	height: auto;
 	object-fit: cover;
 }
 
@@ -58,6 +63,19 @@ export default {
 	border-radius: 0 100px 100px 0;
 }
 
+.feature-item .feature-illustration.illustration-wrapper {
+	padding-right: 32px;
+}
+
+.feature-item .img {
+	padding-left: 32px;
+}
+
+.feature-item .illustration-wrapper img {
+	object-fit: contain;
+	height: 100%;
+}
+
 @media screen and (min-width: 480px) {
 	.illustration-wrapper {
 		height: 480px;
@@ -70,9 +88,27 @@ export default {
 	.illustration-wrapper.right::after {
 		border-radius: 0 200px 200px 0;
 	}
+
+	.feature-item .feature-illustration.illustration-wrapper {
+		padding-right: 0;
+	}
+
+	.feature-item .img {
+		padding-left: 32px;
+		max-width: 90%;
+	}
+
+	.feature-item .illustration-wrapper img {
+		object-fit: contain;
+		max-height: 90%;
+	}
 }
 
 @media screen and (min-width: 1024px) {
+	.illustration-wrapper {
+		min-width: 764px;
+	}
+
 	.illustration-wrapper .img {
 		position: absolute;
 	}
@@ -84,9 +120,24 @@ export default {
 	.illustration-wrapper.right .img {
 		left: 80px;
 	}
+}
 
-	.illustration-wrapper::after {
-		max-width: 492px;
+@media screen and (min-width: 1280px) {
+	.feature-item .img {
+		padding-left: 0;
+	}
+
+	.feature-item .illustration-wrapper img {
+		height: 100%;
+	}
+
+	.feature-item.item-1 .illustration-wrapper .img {
+		left: 140px;
+	}
+
+	.feature-item.item-2 .illustration-wrapper .img,
+	.feature-item.item-3 .illustration-wrapper .img {
+		left: 330px;
 	}
 }
 </style>
