fdc6a06b9a5b3d25a4bd2df08b2bf2bb32719a30
Angel Figuera <angel.figuera@osanasalud.com>
2021-02-11 11:22:06 -0400

    feat: create the app footer component and it's styles

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 	<div id="app">
 		<app-header />
 		<app-main />
+		<app-footer />
 	</div>
 </template>
 
@@ -9,9 +10,10 @@
 import '@/assets/styles/App.css';
 import AppHeader from '@/components/Header/Header.vue';
 import AppMain from '@/components/Main/Main.vue';
+import AppFooter from '@/components/Footer/Footer.vue';
 
 export default {
-	components: { AppHeader, AppMain },
+	components: { AppHeader, AppMain, AppFooter },
 	name: 'App',
 };
 </script>
